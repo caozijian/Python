1c0c21d54130ae90da816c9ab63c10904bceff8f
Zijian Cao <caozijian.cn@gmail.com>
2015-06-02 23:11:52 +0800

    Chapter 3 Try except

--- CH3.py
+++ CH3.py
@@ -1,8 +1,20 @@
 data = open('sketch.txt')
 for each_line in data:
-    (role,line_spoken) = each_line.split(':')
-    print(role,end='')
-    print(' said: ',end='')
-    print(line_spoken,end='')
-    print(each_line,end='')
-the_file.close()
+    # This version for find :
+    '''
+    if not each_line.find(':') == -1:
+        (role,line_spoken) = each_line.split(':',1)
+        print(role,end='')
+        print(' said: ',end='')
+        print(line_spoken,end='')
+        print(each_line,end='')
+    '''
+    try:
+        (role,line_spoken) = each_line.split(':',1)
+        print(role,end='')
+        print(' said: ',end='')
+        print(line_spoken,end='')
+        print(each_line,end='')
+    except:
+        pass
+data.close()
