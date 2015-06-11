90acc788a4ca4016df4d58a100067ac4bf6cba0b
ZijianCao <caozijian.cn@gmail.com>
2015-06-11 13:57:43 +0800

    Chapter 6 Class bundle data and method.

--- CH6.py
+++ CH6.py
@@ -8,16 +8,31 @@ def sanitize(time_string):
     (mins,secs) = time_string.split(splitter)
     return(mins + '.' +secs)
 
+
+
+class Athlete:
+    def __init__(self,a_name,a_dob=None,a_times=[]):
+        # The code to initialize a "Athlete" object.
+        self.name = a_name
+        self.dob = a_dob
+        self.times = a_times        
+
+    def top3(self):
+        return(sorted(set([sanitize(t) for t in self.times]))[0:3])
+
+sarah = Athlete('Sarah Sweeney','2002-6-17',['2:58','2.58','1.56'])
+james = Athlete('James Jones')
+
 def get_coach_data(filename):
     try:
         with open(filename) as f:
             data = f.readline()
-            return(data.strip().split(','))
+        templ = data.strip().split(',')
+        return(Athlete(templ.pop(0),templ.pop(0),templ))
     except IOError as ioerr:
         print('File error: ' + str(ioerr))
         return(None)
 
-sarah = get_coach_data('sarah2.txt')
-(sarah_name,sarah_dob)=sarah.pop(0),sarah.pop(0)
-print(sarah_name + "'s fastest times are: " +
-      str(sorted(set([sanitize(t) for t in sarah]))[0:3]))
+james = get_coach_data('james2.txt')
+
+print(james.name + "'s fastest times are: " + str(james.top3()))
