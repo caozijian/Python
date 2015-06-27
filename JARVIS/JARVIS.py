5c01c0f67f29fd99ce5e07b267bacf167835b71a
ZijianCao <caozijian.cn@gmail.com>
2015-06-27 22:38:32 +0800

    2 bits Data store

--- JARVIS/JARVIS.py
+++ JARVIS/JARVIS.py
@@ -1,13 +1,17 @@
 #coding=utf-8
 ##urllib2 documentation at "https://docs.python.org/2/library/urllib2.html"
+#Requirements:
+#1 Get all file path
+
 import urllib.request
 import urllib.error
-
+import 
 def getHtml(url):
     page = urllib.request.urlopen(url)
     html = page.read()
     return html
-html = getHtml("http://www.cpsri.com.cn")
+html = getHtml("http://www.jd.com")
+
 fin = open("temp.txt",'w')
 fin.write(str(html))
 fin.close()
