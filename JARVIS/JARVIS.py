bf90317d2d30423b7d2a00b423b21ac4ca19f5b0
ZijianCao <caozijian.cn@gmail.com>
2015-06-30 23:41:51 +0800

    Add two file extension name to match list.

--- JARVIS/JARVIS.py
+++ JARVIS/JARVIS.py
@@ -3,6 +3,7 @@
 #Requirements:
 #1 Get all file path
 ##0629 so tired.today
+##Get two type of file list 
 import urllib.request
 import urllib.error
 import re
@@ -21,7 +22,15 @@ def getHtml(url):
 html = getHtml("http://www.python.org")
 
 def getImg(html):
-    reg = r'href="(.+?\.css)"'
+##Python regular expression
+##The special characters are:
+##'.'(Dot.) In the default mode, this matches any character except a newline. If the DOTALL flag has been specified, this matches any character including a newline.
+##    '^'(Caret.) Matches the start of the string, and in MULTILINE mode also matches immediately after each newline.
+##    '$'Matches the end of the string or just before the newline at the end of the string, and in MULTILINE mode also matches before a newline. foo matches both ‘foo’ and ‘foobar’, while the regular expression foo$ matches only ‘foo’. More interestingly, searching for foo.$ in 'foo1\nfoo2\n' matches ‘foo2’ normally, but ‘foo1’ in MULTILINE mode; searching for a single $ in 'foo\n' will find two (empty) matches: one just before the newline, and one at the end of the string.
+##    '*'Causes the resulting RE to match 0 or more repetitions of the preceding RE, as many repetitions as are possible. ab* will match ‘a’, ‘ab’, or ‘a’ followed by any number of ‘b’s.
+##    '+'Causes the resulting RE to match 1 or more repetitions of the preceding RE. ab+ will match ‘a’ followed by any non-zero number of ‘b’s; it will not match just ‘a’.
+##    '?'Causes the resulting RE to match 0 or 1 repetitions of the preceding RE. ab? will match either ‘a’ or ‘ab’.*?, +?, ??The '*', '+', and '?' qualifiers are all greedy; they match as much text as possible. Sometimes this behaviour isn’t desired; if the RE <.*> is matched against '<H1>title</H1>', it will match the entire string, and not just '<H1>'. Adding '?' after the qualifier makes it perform the match in non-greedy or minimal fashion; as few characters as possible will be matched. Using .*? in the previous expression will match only '<H1>'.
+    reg = r'href="(.+?\.css)|(.+?\.js)"'
     imgre = re.compile(reg)
     imglist = re.findall(imgre,html)
     return imglist
